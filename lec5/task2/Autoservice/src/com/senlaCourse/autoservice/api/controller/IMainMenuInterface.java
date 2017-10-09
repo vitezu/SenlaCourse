8f6f5fabb8b71567714bcb151b5af9cf4dfa80ce
Zubritsky.Pavel <zubritsky.pavel@gmail.com>
2017-10-09 04:23:38 +0300

    del

--- lec5/task2/Autoservice/src/com/senlaCourse/autoservice/api/controller/IMainMenuInterface.java
+++
@@ -1,9 +0,0 @@
-package com.senlaCourse.autoservice.api.controller;
-
-public interface IMainMenuInterface {
-
-    void showActionObjectMenu();
-    void showOrderServiceMenu();
-    void showPlaceServiceMenu();
-    void showMasterServiceMenu();
-}
