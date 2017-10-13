1819b99fa9bf4dccc7adcbcd58a144223ae7d89d
Pavel_Z <zubrytski.pavel@gmail.com>
2017-10-14 01:55:45 +0300

    up

--- lec5/task2/AutoserviceNewView/View/src/actions/actionObject/ActionDeletePlace.java
+++ lec5/task2/AutoserviceNewView/View/src/actions/actionObject/ActionDeletePlace.java
@@ -1,11 +1,26 @@
 package actions.actionObject;
 
+import actions.actionsService.ShowActionObjectsMenu;
 import api.IAction;
+import com.senlaCourse.autoservice.entity.Place;
+import com.senlaCourse.autoservice.service.PlaceServiceImpl;
+
+import java.util.Scanner;
 
 public class ActionDeletePlace implements IAction {
 
     @Override
     public void execute() {
+        System.out.println("Enter uniq number of place for deleting");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
+        Place place = new Place(number);
+
+        PlaceServiceImpl.getInstance().deletePlace(place);
+
+        ShowActionObjectsMenu actionObjectsMenu = new ShowActionObjectsMenu();
+        actionObjectsMenu.execute();
 
     }
 
